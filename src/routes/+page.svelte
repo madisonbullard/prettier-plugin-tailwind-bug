d35b3a594996acfc2bacae2db64ec4428ca4930c
Madison Bullard <madison.bullard@gmail.com>
2024-07-03 12:34:22 -0400

    repro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class={'relative flex items-center rounded-full border border-gray-200 bg-gray-100 text-gray-500 shadow-sm transition-colors ' +
+	true
+		? `border-yellow-300 bg-yellow-200 text-yellow-700
+             hover:border-yellow-200 hover:bg-yellow-100 
+             focus:border-yellow-200 focus:bg-yellow-100
+             active:border-yellow-50 active:bg-yellow-50`
+		: `hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700
+			focus:border-gray-100 focus:bg-gray-50 focus:text-gray-700
+			active:border-gray-50 active:bg-white active:text-gray-300`}
+>
+	<button class="flex min-h-5 min-w-5 items-center justify-center rounded-full" aria-label="Menu">
+		hi
+	</button>
+</div>
